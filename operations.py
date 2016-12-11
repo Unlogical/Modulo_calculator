506e0e8e3f8c4f8985391914f28d2ad6996f6884
Unlogical <sakujo8744@mail.ru>
2016-12-12 01:28:52 +0300

    only right results in sqrt.

--- operations.py
+++ operations.py
@@ -55,4 +55,4 @@ def modular_sqrt(a, p):  # 10 mod 53 = 13
         x = prime_mod_sqrt(a, i)
         res.append(x)
         res.append(i - x)
-    return {x for x in res if pow(x, 2, p) == a}
+    return {x for x in res if pow(x, 2, p) == a % p}
